864da1cb17c36ad39093116e8f27adc6fb1109bd
Paul Johansen <paul.d.johansen@gmail.com>
2024-08-06 13:03:23 -0500

    Refactored header generation and added prompting for MUT

--- apple_music_client.py
+++ apple_music_client.py
@@ -8,6 +8,7 @@ load_dotenv()
 
 key_id = os.environ.get("APPLE_KEY_ID")
 team_id = os.environ.get("APPLE_TEAM_ID")
+KEY_FILE_LOCATION= "keys/AuthKey_YJQZ9U3UTA.p8"
 
 MUT_GENERATOR = "https://d1skmsh1xucp4e.cloudfront.net/index.html"
 BASE_API_URL = "https://api.music.apple.com/v1/"
@@ -15,16 +16,27 @@ TEST_API_ENDPOINT = BASE_API_URL + "test"
 SEARCH_API_ENDPOINT = BASE_API_URL + "catalog/us/search"
 MUSIC_USER_TOKEN = "AmxtjdXEbzvLiq2+DYqUHgEbcJo/weuLRGWda2aH6l4hn4y/MOwcSh4A6mvGrMLlPBEAP2c+P9YEPWLaE4qIAd+ad+l6NASY79e8F2DGIli9+SB8+MO6tMfwm0Erb4g5UIx2JmwrfESEyhuXUyEtzHxWU5lbfLzm/DnRRnpCCMLsInSCMYsNlavzVZybsoTcrxrnEB1RVYrvPibi2174XiQZznzydyXOtOSMtVmPUu2248ffRA=="
 PLAYLISTS_API_ENDPOINT = BASE_API_URL + "me/library/playlists"
+APP_ID = "VHARX7K299.media.johansenet.setlisttoplaylist"
 
 
 class AppleMusicClient:
     def __init__(self):
         self.developer_token = ""
         self._alg = "ES256"
-        with open("AuthKey_YJQZ9U3UTA.p8", "r") as f:
+        with open(KEY_FILE_LOCATION, "r") as f:
             self.private_key = f.read()
         self._generate_am_token(12)
         self.mut_generator_url = MUT_GENERATOR
+        self.app_id = APP_ID
+        self.music_user_token = MUSIC_USER_TOKEN
+
+    def generate_headers(self, include_music_user_token: bool):
+        playlist_headers = {}
+        playlist_headers["Authorization"] = f"Bearer {self.developer_token}"
+        if include_music_user_token:
+            playlist_headers["Music-User-Token"] = self.music_user_token
+        return playlist_headers
+
 
     def search_for_song(self, song_title, artist):
         search_url = SEARCH_API_ENDPOINT + "?"
@@ -34,18 +46,22 @@ class AppleMusicClient:
 
         response = requests.get(
             url=search_url,
-            headers=self.auth_headers,
+            headers=self.generate_headers(False),
         )
         response.raise_for_status()
         search_results = response.json()
+        print(search_results["results"]["songs"]["data"][0]["attributes"]["name"])
 
     def run_test(self):
         response = requests.get(
             url=TEST_API_ENDPOINT,
-            headers=self.auth_headers
+            headers=self.generate_headers(False),
         )
         response.raise_for_status()
         print(response.status_code)
+
+    def set_music_user_token(self, token):
+        self.music_user_token = token
 
     def _generate_am_token(self, session_length):
         token_exp_time = datetime.now() + timedelta(hours=session_length)
@@ -58,19 +74,12 @@ class AppleMusicClient:
         self.token_valid_until = token_exp_time
         token = jwt.encode(payload, self.private_key, algorithm=self._alg, headers=headers)
         self.developer_token = token if type(token) is not bytes else token.decode()
-        self.auth_headers = {
-            "Authorization": f"Bearer {self.developer_token}"
-        }
 
     def get_all_playlists(self):
         playlist_url = PLAYLISTS_API_ENDPOINT
         playlist_url = playlist_url
 
-        playlist_headers = {
-            "Authorization": f"Bearer {self.developer_token}",
-            "Music-User-Token": MUSIC_USER_TOKEN
-
-        }
+        playlist_headers = self.generate_headers(True)
 
         response = requests.get(
             url=playlist_url,
--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 from setlist_grabber import SetlistGrabber
 from apple_music_client import AppleMusicClient
 import urllib.parse
+import pyperclip
 
-APP_ID = "VHARX7K299.media.johansenet.setlisttoplaylist"
+URL="https://www.setlist.fm/setlist/crown-the-empire/2024/the-regent-theater-los-angeles-ca-7355f209.html"
+ARTIST="Crown The Empire"
 
+def prompt_for_next():
+    input("Ready to continue?")
 
+def prompt_for_MUT():
+    pyperclip.copy(amc.mut_generator_url)
+    print(f"URL copied to clipboard: {amc.mut_generator_url}")
+    prompt_for_next()
+    pyperclip.copy(amc.developer_token)
+    print(f"Developer token copied to clipboard: {amc.developer_token}")
+    prompt_for_next()
+    pyperclip.copy(amc.app_id)
+    print(f"App Id to clipboard: {amc.app_id}")
+    amc.set_music_user_token(input("Enter your Music User Token"))
 
-URL="https://www.setlist.fm/setlist/crown-the-empire/2024/lovedrafts-brewing-co-mechanicsburg-pa-6ba9a616.html"
-
-grabber = SetlistGrabber(URL)
-songs = grabber.get_song_names()
+# grabber = SetlistGrabber(URL)
+# songs = grabber.get_song_names()
 
 
 amc = AppleMusicClient()
 amc.get_all_playlists()
+
+
+
 # for song in songs:
 #     print(song)
-#     amc.search_for_song(song, "Crown The Empire")
+#     amc.search_for_song(song, ARTIST)
 
 
 
